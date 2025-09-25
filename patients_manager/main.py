1282fe6fc0c994bbab24a455b1d70666ed97d09e
kamatealif <kamatealip1@gmail.com>
2025-09-25 23:31:45 +0530

    create patent route

--- patients_manager/main.py
+++ patients_manager/main.py
@@ -1,104 +1,107 @@
-import uvicorn
-from fastapi import FastAPI,Path, HTTPException, Query
+from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
-
-
+from typing import Annotated, Literal, Optional
 import json
 
-# pydantic model to data validation 
-class Patient(BaseModel):
-    # Unique identifier for the patient
-    patient_id: Annotated[str, Field(..., description="Unique identifier for the patient", examples=['p001'])]
-    
-    # Name of the patient
-    name: Annotated[str, Field(..., description="Name of the patient", examples=['John Doe'])]
-    
-    # Weight of the patient in kilograms
-    weight: Annotated[float, Field(..., description="Weight of the patient in kilograms", examples=[70.0])]
-    
-    # Height of the patient in centimeters
-    height: Annotated[float, Field(..., description="Height of the patient in centimeters", examples=[170.0])]
-    
-    # City of the patient
-    city: Annotated[str, Field(..., description="City of the patient", examples=['New York'])]
-    
-    # Email of the patient
-    email: Annotated[str, Field(..., description="Email of the patient", examples=['john.doe@example.com'])]
-    
-    # Phone number of the patient
-    phone: Annotated[str, Field(..., description="Phone number of the patient", examples=['123-456-7890'])]
-    
-    # Age of the patient
-    age: Annotated[int, Field(..., description="Age of the patient", examples=[25])]
-    
-    # Gender of the patient
-    gender: Annotated[Literal['male','female'], Field(..., description="Gender of the Patient", examples=['male'])]
-    
-    # calculating bim
-    @computed_field
-    @property
-    def bmi(self)-> float:
-        height = self.height/ 100
-        bmi = round(self.weight/ (height**2),2)
-        return bmi
-
-    @computed_field
-    @property
-    def verdict_prediction(self) -> str:
-        if self.bmi < 18.5:
-            return "Under weight"
-        elif  self.bmi >= 18.5 and self.bmi <= 25:
-            return "Normal"
-        elif self.bmi >= 25 and self.bmi <= 29.9:
-            return "Over weight"
-        elif self.bmi >= 30 and self.bmi <= 34.9:
-            return "class I obesity"
-        elif self.bmi <= 35 and self.bmi <= 39.9:
-            return "class II obesity"
-        elif self.bmi >= 40:
-            return "class III obesity"
-    
-    
 app = FastAPI()
 
+class Patient(BaseModel):
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+     
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
+
     return data
 
+def save_data(data):
+    with open('patients.json', 'w') as f:
+        json.dump(data, f)
+        
+
 @app.get("/")
-def home():
-    return {"message": "Hello World"}
+def hello():
+    return {'message':'Patient Management System API'}
 
-@app.get("/patients")
-def patients(): 
+@app.get('/about')
+def about():
+    return {'message': 'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
     data = load_data()
-    return data 
 
+    return data
 
-@app.get("/patient/{patient_name}")
-def view_patient(patient_name : str = Path(..., description="Name of the patient", examples="John Doe")):
-    data = load_data();
-    for patient in data:
-        if patient["name"] == patient_name:
-            return patient
-    else:
-        raise HTTPException(status_code=404, detail="Patient not found")
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
 
 @app.get('/sort')
-def sort_by(sort_by: str = Query(..., description="Sort by name, age, height, weight, bmi", examples="name"), order: str = Query('asc', description="Order by asc or desc", examples="asc")):
-    valid_fields = ['name','age,','height','weight','bmi']
-    if sort_by not in valid_fields:
-        raise HTTPException(status_code=400, detail=f"Invalid field, select from {valid_fields}")
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
 
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+    
     if order not in ['asc', 'desc']:
-        raise HTTPException(status_code=400, details=f"Invalid order, select from asc or desc")
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
     data = load_data()
 
-    sorted_data = sorted(data, key=lambda x: x[sort_by], reverse=(order == 'desc'))
+    sort_order = True if order=='desc' else False
 
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
 
     return sorted_data
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+@app.post('/create')
+def create_patient(patient: Patient):
+
+    # load existing data
+    data = load_data()
+
+    # check if the patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient already exists')
+
+    # new patient add to the database
+    data[patient.id] = {k: v for k, v in patient.dict().items() if k != 'id'}
+
+    # save into the json file
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
