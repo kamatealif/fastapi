94369519b50acb43f2b102335ede4fd142adbe51
kamatealif <kamatealip1@gmail.com>
2025-10-05 21:08:00 +0530

    imprv

--- tiangolo/main.py
+++ tiangolo/main.py
@@ -4,6 +4,20 @@ items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'Baz'}]
 
 app = FastAPI()
 
-@app.get("/items/")
-async def read_items(skip:int = 0, limit:int = 10):
-    return items_db[skip: skip + limit]
+@app.get("/user/{user_id}/items/{item_id}")
+async def read_items(user_id: str, item_id: str , q: str | None =  None, short: bool = False):
+    item = {"item_id": item_id, "Owner id " : user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+         item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item 
+    
+    
+    
+@app.get('/items/{item_id}')
+async def read_item(item_id: str, needy: str, skip=0, limit=10):
+    item = {"item_id": item_id, "needy":  needy, "skip": skip, "limit": limit}
+    return item
